6bdda484b8a83ab99bed6d56aa63f5598007da10
shahzaibkhan2 <drshahzeb47@gmail.com>
2024-05-21 17:05:15 +0500

    changes in recent project

--- src/components/workDetails/WorkDetails.jsx
+++ src/components/workDetails/WorkDetails.jsx
@@ -7,12 +7,14 @@ function WorkDetails() {
         <h2 className={styles.heading}>YOUTUBE CLONE</h2>
         <p className={styles.sub__heading}>
           I Built a fully functional, responsive and beautiful YouTube Clone
-          using JavaScript, HTML and CSS, Git and Github, Redux and ECMA Script
-          on ReactJs. I fetched the YouTube API using advanced JavaScript async
-          await methods and ECMA Script methods. This website is available on my
-          Github ID. I have also hosted this website on Vercel at this address
-          https://shahzaib-you-tube-clone.vercel.app to make it available to the
-          public
+          using JavaScript, HTML and CSS, Git and Github, Redux Toolkit and ECMA
+          Script on ReactJs.
+        </p>
+        <p>
+          I fetched the YouTube API using advanced JavaScript async await
+          methods and ECMA Script methods. This website is available on my
+          Github ID. I have also hosted this website on Vercel to make it
+          available to the public
         </p>
         <button>
           <a target="_blank" href="https://shahzaib-you-tube-clone.vercel.app">
